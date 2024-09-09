86e847df9a79dc821016172c181504c2b4117ea2
Juan David Camelo <jdavidcamelo94@gmail.com>
2024-09-09 11:34:58 +0000

    Fix requirements and add rumtimetxt

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import env
 import dj_database_url
 
 
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9j#g2e3yxrv_yya*564!tfs(bllbb!3o7flfz55_ixmpn_h)br'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
